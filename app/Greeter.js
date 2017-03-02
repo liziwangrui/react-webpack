968c98ef0c961d13c3794fe7fece352f5507dea9
wangrui <wangrui@innobuddy.com>
2017-03-02 15:32:33 +0800

    添加react

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,15 @@
 // Greeter.js
-var configJson = require('./config.json');
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = configJson.greetText;
-  return greet;
-};
+// var configJson = require('./config.json');
+// module.exports = function() {
+//   var greet = document.createElement('div');
+//   greet.textContent = configJson.greetText;
+//   return greet;
+// };
+import React from 'react';
+import configJson from './config.json';
+class Greeter extends React.Component {
+  render() {
+    return <div>{configJson.greetText}</div>
+  }
+}
+export default Greeter;
