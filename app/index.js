d9070be43f0e68907fd8ea06e4414d5fd39e890c
wangrui <wangrui@innobuddy.com>
2017-03-03 15:18:07 +0800

    react-redux todo-list

--- app/index.js
+++ app/index.js
@@ -2,9 +2,26 @@
 // var greeter = require('./Greeter.js');
 // document.getElementById('root').appendChild(greeter());
 
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter';
-import './main.css';
+// import React from 'react';
+// import {render} from 'react-dom';
+// import Greeter from './Greeter';
+// import './main.css';
+//
+// render(<Greeter />, document.getElementById('root'));
 
-render(<Greeter />, document.getElementById('root'));
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
